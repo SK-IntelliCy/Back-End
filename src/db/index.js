ba557e03871f1c986ebf12955781ca6b64599506
SK-IntelliCy <skintellicy@gmail.com>
2024-09-30 22:10:46 +0530

    Database Conected

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+
+
+
+const connectDB =async ()=>{
+    try{
+        const ConectionInstance =await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log(`\n conected , DB Host -:${ConectionInstance.Connection.host}`)
+    }catch (error){
+        console.log(error,'error')
+        process.exit(1)
+    }
+}
+
+export default connectDB;
+
+
+// import { DB_Name } from "../constants.js";

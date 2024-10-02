07f212f48cc9098183401e6bc4b5e99fd99e73a7
SK-IntelliCy <skintellicy@gmail.com>
2024-10-02 21:11:01 +0530

    add-user-and-video-model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose ,{Schema, Types}from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { JsonWebTokenError } from "jsonwebtoken";
+import bcrypt from "bcrypt"
+const videoSchema=new Schema(
+    {   
+        videoFile:{
+            type:String,
+            required:true,
+        },
+        thumbnail:{
+            type:String,
+            required:true,
+        },
+        title:{
+            type:String,
+            required:true,
+        },
+        discription:{
+            type:String,
+            required:true,
+        },
+        duraqtion:{
+            type: String,
+            required:true,
+        },
+        views:{
+            type: Number,
+            default:0,
+        },
+        isPublished:{
+            type: Boolean,
+            default:true,
+        },
+        Owner:{
+            type: Schema.Types.ObjectId,
+            ref:"User"
+        }
+    },
+    {timestamps:true}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Video=mongoose.model("Video",videoSchema)
